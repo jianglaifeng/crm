034a598332c7978d9b0190bcc93c867d627e4620
jianglaifeng <1328654954@qq.com>
2020-08-28 19:35:00 +0800

    登录完成

---
+++ js/login.js
@@ -0,0 +1,37 @@
+$(function(){
+    //登录功能
+    $(".submit").click(async function(e){
+        let account = $(".userName").val().trim()
+        let password = $(".userPass").val().trim()
+
+        if(account === "" || password === ""){
+            return alert("账户和密码不能为空")
+        }
+        //可以自己通过正则自己校验你的用户名和密码  test
+        password = md5(password)
+        // console.log(account,password);        
+
+        //发送ajax请求
+        // axios.post("/user/login",{
+        //     account,
+        //     password
+        // }).then(res=>{
+        //     console.log(res);
+        // }).catch(err=>{
+        //     console.log(err);
+        // })
+
+        let res = await axios.post("/user/login",{account,password})
+        // console.log(res);
+        if(parseInt(res.code) ===0){
+            alert("登录成功")
+            window.location.href="index.html"
+            return
+        }
+        alert("用户名和密码出错了")
+    })
+
+
+
+
+})
