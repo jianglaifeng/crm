d6ab73fc566bd209114afd2e7739a05e490ef70d
jianglaifeng <1328654954@qq.com>
2020-08-31 14:10:07 +0800

    完成用户信息

--- js/axios.defaults.js
+++ js/axios.defaults.js
@@ -1,5 +1,8 @@
 //对axios进行二次封装
-axios.defaults.baseURL = "http://localhost:8888" //配置请求的基本路径
+// axios.defaults.baseURL = "http://localhost:8888" //配置请求的基本路径
+axios.defaults.baseURL = "http://127.0.0.1:8888"
+axios.defaults.withCredentials =true  //配置为true  后台的请求会带上cookie
+
 //数据以表单的形式扔给服务器
 axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
 //还是以表单的形式扔给服务器 数据格式是这样的：name=z3&age=4
@@ -7,7 +10,7 @@ axios.defaults.transformRequest = function (data) {
     if(!data) return data 
     let result = ''
     for (let attr in data) {
-        if(!data.hasOwnProperty(attr)) break
+        if(!data.hasOwnProperty(attr)) break 
         result += `&${attr}=${data[attr]}`
     }
     return result.substring(1)
